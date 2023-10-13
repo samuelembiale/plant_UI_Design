eada5d71692808e82a5d0d5aeef69368d81a729f
Samuel Embiale <samuelembiale13@gmail.com>
2023-10-13 11:18:49 +0300

    first message

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:plant_ui_design/onbaording.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const Onboarding(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
---
+++ lib/onbaording.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OnBoardingSlider(
+        headerBackgroundColor: Colors.white,
+        finishButtonText: 'Register',
+        finishButtonStyle: const FinishButtonStyle(
+          backgroundColor: Colors.black,
+        ),
+        skipTextButton: const Text('one'),
+        trailing: const Text('Login'),
+        background: [
+          Image.asset('assets/Image/plant.png'),
+          Image.asset('assets/Image/plant.png'),
+        ],
+        totalPage: 2,
+        speed: 1.8,
+        pageBodies: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: const Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 200,
+                ),
+                Text('Description Text 2'),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: const Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 200,
+                ),
+                Text('Description Text 2'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
